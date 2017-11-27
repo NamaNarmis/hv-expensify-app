48f0747cd82495999ba0786c176c391e940e46a5
Harvinder Vasir <hvasir@Harvinders-Mac-mini.local>
2017-11-26 19:20:02 -0500

    small chnage

--- src/app.js
+++ src/app.js
@@ -11,14 +11,6 @@ import './styles/styles.scss';
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'Water bill' }));
-store.dispatch(addExpense({ description: 'Gas bill' }));
-store.dispatch(setTextFilter('water'));
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
-
 const jsx = (
     <Provider store={store}>
         <AppRouter />
